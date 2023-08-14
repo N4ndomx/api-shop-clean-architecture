bc6925afd0d81c1c687b7896a663049dabbdb821
Fernando Ramirez <90127607+N4ndomx@users.noreply.github.com>
2023-08-14 00:08:32 -0600

    add validacion-dtos and conexion TypeOrm-Mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ProductModule } from './case-uses/product/product.module';
 import { DataServiceModule } from './services/data-service/data-service.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ProductModule, DataServiceModule,],
+  imports: [
+    ConfigModule.forRoot(),
+    ProductModule, DataServiceModule,],
   providers: [],
 })
 export class AppModule { }
--- src/services/data-service/data-service.module.ts
+++ src/services/data-service/data-service.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
-import { LocalStorageModule } from '../../frameworks/data-services/local-storage/local-storage.module';
+// import { LocalStorageModule } from '../../frameworks/data-services/local-storage/local-storage.module';
+import { TypeormModule } from '../../frameworks/data-services/typeorm/typeorm.module';
 
 @Module({
-    imports: [LocalStorageModule],
-    exports: [LocalStorageModule]
+    imports: [TypeormModule],
+    exports: [TypeormModule]
 })
 export class DataServiceModule { }
